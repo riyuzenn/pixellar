32b41361672cb4a9ab31f51bbcb15fd5ceffdb53
riyuzenn <riyuzenn@gmail.com>
2023-04-07 21:20:18 +0800

    add debugging

--- crates/lime/src/main.rs
+++ crates/lime/src/main.rs
@@ -1,9 +1,10 @@
 use std::net::Ipv4Addr;
 use clap::Parser;
-use anyhow::{
-    Result,
-    Context
-};
+use fern::colors::{Color, ColoredLevelConfig};
+use log::{debug, info, warn};
+use colored::Colorize;
+use anyhow::Result;
+use std::time::Duration;
 
 mod args;
 mod utils;
@@ -25,9 +26,16 @@ fn main() -> Result<()> {
     let size = arg.size.or(Some(10)).unwrap();
     let ver = arg.ver.or(Some(env!("CARGO_PKG_VERSION").to_string())).unwrap();
     
+    if debug {
+        setup_logging();
+    } else {
+        warn!("Please use debugger to debug the server when it crashed");
+    }
+    
     let addr = Address::new(&host, port);
-
     let version = Version::from(&ver)?;
+    
+    debug!("Creating Server object");
     let server = Server::new(
         addr.host,
         addr.port,
@@ -35,6 +43,69 @@ fn main() -> Result<()> {
         debug,
         version
     );
+    info!("Server object created");
 
     Ok(())
+}
+
+fn setup_logging() {
+    // configure colors for the whole line
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        // we actually don't need to specify the color for debug and info, they are white by default
+        .info(Color::White)
+        .debug(Color::BrightBlack);
+    // configure colors for the name of the level.
+    // since almost all of them are the same as the color for the whole line, we
+    // just clone `colors_line` and overwrite our changes
+    let colors_level = colors_line.clone().info(Color::Green);
+    
+    let file = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(fern::log_file("server.log").unwrap());
+
+
+
+
+    // here we set up our fern Dispatch
+    let stdout = fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
+                color_line = format_args!(
+                    "\x1B[{}m",
+                    colors_line.get_color(&record.level()).to_fg_str()
+                ),
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
+        })
+        
+        // change log levels for individual modules. Note: This looks for the record's target
+        // field which defaults to the module path but can be overwritten with the `target`
+        // parameter:
+        // `info!(target="special_target", "This log message is about special_target");`
+        // .level_for("pretty_colored", log::LevelFilter::Trace)
+        // output to stdout
+        .chain(std::io::stdout());
+
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Trace)
+        .chain(file)
+        .chain(stdout)
+        .apply()
+        .unwrap();
+
+    debug!("Logging setup finisehd");
 }
