39ad08fc373c4b4f71f609c9a78e096239a53cf7
riyuzenn <riyuzenn@gmail.com>
2023-04-07 21:24:15 +0800

    update server

--- crates/lime/src/server.rs
+++ crates/lime/src/server.rs
@@ -1,8 +1,14 @@
 use enet::*;
 use std::{net::Ipv4Addr, time::Duration};
 use anyhow::Context;
-
+use colored::Colorize;
 use crate::utils::Version;
+use log::{
+    warn,
+    info,
+    debug,
+    error
+};
 
 pub struct Server {
     host: Ipv4Addr,
@@ -11,6 +17,14 @@ pub struct Server {
     enet: Enet,
     debug: bool,
     version: Version
+}
+
+#[allow(dead_code)]
+enum Log {
+    INFO,
+    DEBUG,
+    WARN,
+    ERROR
 }
 
 impl Server {
@@ -30,6 +44,78 @@ impl Server {
             debug: debug,
             version: v,
             enet: enet_obj
+        }
+    }
+
+    pub fn run_server(&self, duration: u64) {
+        let mut host = self.enet.create_host::<()>(
+
+            Some(&Address::new(self.host, self.port)),
+            self.peer_count,
+            ChannelLimit::Maximum,
+            BandwidthLimit::Unlimited,
+            BandwidthLimit::Unlimited
+
+        ).context("Failed to create ENet host").unwrap();
+        host.set_checksum_crc32(); 
+        
+        self.log(
+            &format!("Server service {0}. Listening to {1}", 
+                "started".green(), 
+                "events".yellow()
+            ), 
+            Log::INFO
+        );
+
+        loop {
+        
+            if let Some(event) = host
+                .service(Duration::from_secs(duration))
+                .context("Service failure").unwrap()
+
+            {
+                self.log("Receive new event", Log::INFO);
+                match event.r#type() {
+                    
+                    &EventType::Connect => self.handle_connection(event),
+                    &EventType::Disconnect { .. } => println!("disconnect!"),
+                    &EventType::Receive {
+                        ref channel_id,
+                        ref packet,
+                    } => println!(
+                        "got packet on channel {}, content: '{}'",
+                        channel_id,
+                        std::str::from_utf8(packet.data()).unwrap()
+                    ),
+                }
+            }
+        }
+        
+    }
+
+    /// When the client first connect, send a handshake logic
+    /// to determine if the connection is secure and legitimate.
+    fn handle_connection(&self, event: Event<()>) {
+        let peer = event.peer(); 
+        self.log(
+            &format!("New connection: {}:{}", 
+                peer.address().ip(), 
+                peer.address().port()
+            ),
+        Log::INFO);
+        
+    }
+
+    
+
+    fn log(&self, msg: &str, r#type: Log) {
+        if self.debug {
+            match r#type {
+                Log::DEBUG => debug!("{}", msg),
+                Log::WARN => warn!("{}", msg),
+                Log::ERROR => error!("{}", msg),
+                Log::INFO => info!("{}", msg),
+            }
         }
     }
 }
