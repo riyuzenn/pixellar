7b5026bc34f362473ffa8c1b50d625b2d5981150
riyuzenn <riyuzenn@gmail.com>
2023-04-07 21:13:32 +0800

    create host_to_vec fn

--- crates/lime/src/utils/mod.rs
+++ crates/lime/src/utils/mod.rs
@@ -1,3 +1,24 @@
 
+use anyhow::{
+    Result,
+    Context
+};
+
 pub use crate::utils::version::Version;
 pub mod version;
+
+
+pub fn host_to_vec(host: &str) -> Result<Vec<u8>> {
+    let mut h = Vec::new();
+    let p = host.split(".")
+        .collect::<Vec<&str>>();
+    
+    for x in p {
+        h.push(
+            x.parse::<u8>()
+                .context("Failed to convert to u8. Perhaps your parameter is not formatted properly?")?
+        );
+    }
+
+    Ok(h)
+}
