3483d6a2cf912155d628e1f1a3f07cdc7c922972
riyuzenn <riyuzenn@gmail.com>
2023-04-07 20:30:10 +0800

    update lime

--- crates/lime/src/main.rs
+++ crates/lime/src/main.rs
@@ -1,7 +1,14 @@
+use clap::Parser;
 
 mod args;
-
+use crate::args::Arguments;
 
 fn main() {
-    println!("Hello, world!");
+    let arg = Arguments::parse();
+    
+    let host = arg.host.or(Some("0.0.0.0".to_string())).unwrap();
+    let port = arg.port.or(Some(9001)).unwrap();
+    let debug = arg.debug.or(Some(true)).unwrap();
+    let size = arg.size.or(Some(10)).unwrap();
+    let ver = arg.ver.or(Some(env!("CARGO_PKG_VERSION").to_string())).unwrap();
 }
